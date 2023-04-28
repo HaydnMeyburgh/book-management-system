9adfef524253d30ec7b8901ccb2b97d8d99b78fd
Haydn <haydnmey@gmail.com>
2023-04-28 13:22:13 +0200

    created api functions in models file

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -18,3 +18,26 @@ func init() {
 	config.Connect()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreateBook() *Book {
+	db.Create(&b)
+	return b
+}
+
+func GetAlllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
