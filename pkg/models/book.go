58d5619b9f63d0cd9877852646369678dd680411
Haydn <haydnmey@gmail.com>
2023-05-02 07:58:18 +0200

    created init function that gets the db and automigrates the book struct into the db

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"gorm.io/gorm"
+	"github.com/HaydnMeyburgh/booking-management-system/pkg/config"
 )
 
 var DB *gorm.DB
@@ -11,6 +12,12 @@ type Book struct {
 	Name string `gorm:"" json:"name"`
 	Author string `json:"authro"`
 	Publication string `json:"publications"`
+}
+
+func init() {
+	config.DBConnection()
+	db := config.GetDB()
+	db.AutoMigrate(&Book{})
 }
 
 func (b *Book) CreateBook() *Book {
