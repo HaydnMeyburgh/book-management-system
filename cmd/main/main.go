f2ecf9a22eb0e5c7af83b2f50aaa7d8e575daeca
Haydn <haydnmey@gmail.com>
2023-05-02 07:55:34 +0200

    removed the connectDB from main.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,14 +4,11 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/HaydnMeyburgh/booking-management-system/pkg/config"
 	"github.com/HaydnMeyburgh/booking-management-system/pkg/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	config.DBConnection()
-
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
